d17b06146b5858af720b68b02ae886be1065de6a
ChenJiaoZhu <18573318185@163.com>
2022-03-16 19:52:25 +0800

    update address and add tokens

--- networks/fusion_mainnet.js
+++ networks/fusion_mainnet.js
@@ -3,6 +3,7 @@ const reporter_ = "0xC9FaBc6B8C916FB1A2c511E92fd1F61FD5AaFbF8";
 const momaFactory_ = "0x865bB9A28041259b4baDAFD37799A288aAbbfC8c";
 const ethSymbol_ = "FSN";
 
+// contract: branch - only_reporter
 const configs = [
     {
         symbol: "FSN", 
@@ -16,7 +17,7 @@ const configs = [
     },
     {
         symbol: "MOMAT", 
-        tokenAddr: "0xa33d4e49f6FB7b7027b8d8B1211B43d0f325BE43",
+        tokenAddr: "0x48b0632d25dcd9e1cec9eaca7cd97b82c452304f",
         linkOracle: "0x0000000000000000000000000000000000000000",
         uniswapPair: "0x0000000000000000000000000000000000000000",
         pairToken: "0x0000000000000000000000000000000000000000",
@@ -26,7 +27,7 @@ const configs = [
     },
     {
         symbol: "LC", 
-        tokenAddr: "0x664cb7a0a0a86779f1a8748cc02f9872acad3e0a",
+        tokenAddr: "0x11a8e0b37501a76dd25a414b5c7ff885695b0de3",
         linkOracle: "0x0000000000000000000000000000000000000000",
         uniswapPair: "0x0000000000000000000000000000000000000000",
         pairToken: "0x0000000000000000000000000000000000000000",
@@ -34,6 +35,78 @@ const configs = [
         fixedPrice: "0",
         priceSource: "3",
     },
+
+
+    {
+        symbol: "fFSN", 
+        tokenAddr: "0x2b8bb627ce7c0c9cbe579e83099de665032d08be", 
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000000000000000",
+        fixedPrice: "0",
+        priceSource: "3", // REPORTER
+    },
+    {
+        symbol: "CHNG", 
+        tokenAddr: "0x05573124c64c69d85687152b2942bcb0a3b26d99",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000000000000000",
+        fixedPrice: "0",
+        priceSource: "3",
+    },
+    {
+        symbol: "BTC", 
+        tokenAddr: "0x816f67ce73aeb099e894a55d135168dd501b55a6",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "100000000",
+        fixedPrice: "0",
+        priceSource: "3",
+    },
+    {
+        symbol: "ETH", 
+        tokenAddr: "0x796d74a86db307b0b0e02fed9fa19ccb1906ce37",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000000000000000",
+        fixedPrice: "0",
+        priceSource: "3",
+    },
+    {
+        symbol: "USDT", 
+        tokenAddr: "0x9636d3294e45823ec924c8d89dd1f1dffcf044e6",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000",
+        fixedPrice: "100000000",
+        priceSource: "1", // FIXED_USD
+    },
+    {
+        symbol: "DAI", 
+        tokenAddr: "0x947250c8664600b7cb18b0de73e592ed78598b8f",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000000000000000",
+        fixedPrice: "100000000",
+        priceSource: "1", // FIXED_USD
+    },
+    {
+        symbol: "BUSD", 
+        tokenAddr: "0xd169bfa08ae2c9bea4123a8760ba058afd647ed2",
+        linkOracle: "0x0000000000000000000000000000000000000000",
+        uniswapPair: "0x0000000000000000000000000000000000000000",
+        pairToken: "0x0000000000000000000000000000000000000000",
+        baseUnit: "1000000000000000000",
+        fixedPrice: "100000000",
+        priceSource: "1", // FIXED_USD
+    }
 ];
 
 module.exports = {
